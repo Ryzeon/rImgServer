05378f781420dea6f5d205ecf41d0ce55b4cea6b
Ryzeon <alex.ramon2004@gmail.com>
2021-05-31 12:53:26 -0500

    UpdatedA  src/main/java/me/ryzeon/img/Lang.java

---
+++ src/main/java/me/ryzeon/img/Lang.java
@@ -0,0 +1,9 @@
+package me.ryzeon.img;/**
+ * Created by Ryzeon
+ * Project: rImgServer
+ * Date: 31/05/2021 @ 12:53
+ * Twitter: @Ryzeon_ 😎
+ * Github:  github.ryzeon.me
+ */
+    public enum Lang {
+}
