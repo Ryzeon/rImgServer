27fe2b3b5ac88704fcb1ac2e493a9b19d5b8755c
Ryzeon <alex.ramon2004@gmail.com>
2021-05-31 15:51:46 -0500

    Updated  src/main/java/me/ryzeon/img/Start.java

--- src/main/java/me/ryzeon/img/Start.java
+++ src/main/java/me/ryzeon/img/Start.java
@@ -2,8 +2,6 @@ package me.ryzeon.img;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 /**
  * Created by Ryzeon
@@ -20,16 +18,6 @@ import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class Start {
-
-    @Bean
-    public CommonsRequestLoggingFilter requestLoggingFilter() {
-        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
-        loggingFilter.setIncludeClientInfo(true);
-        loggingFilter.setIncludeQueryString(true);
-        loggingFilter.setIncludePayload(true);
-        loggingFilter.setMaxPayloadLength(64000);
-        return loggingFilter;
-    }
 
     public static void main(String[] args) {
         System.getProperties().put("server.port", Lang.PORT.getValue());
