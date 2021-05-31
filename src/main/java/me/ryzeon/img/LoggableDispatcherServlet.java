4dc262fe4e20065aff3a61089320346f27a84846
Ryzeon <alex.ramon2004@gmail.com>
2021-05-31 15:53:00 -0500

    Updated  src/main/java/me/ryzeon/img/LoggableDispatcherServlet.java

--- src/main/java/me/ryzeon/img/LoggableDispatcherServlet.java
+++ src/main/java/me/ryzeon/img/LoggableDispatcherServlet.java
@@ -8,5 +8,61 @@ package me.ryzeon.img;
  * Github:  github.ryzeon.me
  */
 
-public class LoggableDispatcherServlet {
+public class LoggableDispatcherServlet extends DispatcherServlet {
+
+    private final Log logger = LogFactory.getLog(getClass());
+
+    @Override
+    protected void doDispatch(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        if (!(request instanceof ContentCachingRequestWrapper)) {
+            request = new ContentCachingRequestWrapper(request);
+        }
+        if (!(response instanceof ContentCachingResponseWrapper)) {
+            response = new ContentCachingResponseWrapper(response);
+        }
+        HandlerExecutionChain handler = getHandler(request);
+
+        try {
+            super.doDispatch(request, response);
+        } finally {
+            log(request, response, handler);
+            updateResponse(response);
+        }
+    }
+
+    private void log(HttpServletRequest requestToCache, HttpServletResponse responseToCache, HandlerExecutionChain handler) {
+        LogMessage log = new LogMessage();
+        log.setHttpStatus(responseToCache.getStatus());
+        log.setHttpMethod(requestToCache.getMethod());
+        log.setPath(requestToCache.getRequestURI());
+        log.setClientIp(requestToCache.getRemoteAddr());
+        log.setJavaMethod(handler.toString());
+        log.setResponse(getResponsePayload(responseToCache));
+        logger.info(log);
+    }
+
+    private String getResponsePayload(HttpServletResponse response) {
+        ContentCachingResponseWrapper wrapper = WebUtils.getNativeResponse(response, ContentCachingResponseWrapper.class);
+        if (wrapper != null) {
+
+            byte[] buf = wrapper.getContentAsByteArray();
+            if (buf.length > 0) {
+                int length = Math.min(buf.length, 5120);
+                try {
+                    return new String(buf, 0, length, wrapper.getCharacterEncoding());
+                }
+                catch (UnsupportedEncodingException ex) {
+                    // NOOP
+                }
+            }
+        }
+        return "[unknown]";
+    }
+
+    private void updateResponse(HttpServletResponse response) throws IOException {
+        ContentCachingResponseWrapper responseWrapper =
+                WebUtils.getNativeResponse(response, ContentCachingResponseWrapper.class);
+        responseWrapper.copyBodyToResponse();
+    }
+
 }
