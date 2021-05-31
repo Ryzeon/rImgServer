5cf79df68b54991396743f5d052469171a5fc9f8
Ryzeon <alex.ramon2004@gmail.com>
2021-05-31 14:59:20 -0500

    Updated  src/main/java/me/ryzeon/img/rest/rPageErrorController.java   src/main/resources/application.properties

--- src/main/java/me/ryzeon/img/rest/rPageErrorController.java
+++ src/main/java/me/ryzeon/img/rest/rPageErrorController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class rPageErrorController implements ErrorController {
 
-    @RequestMapping("/error")
+    @RequestMapping("/404")
     public String handleError() {
         return "EROR PE CONCHATUMADCRE";
     }
