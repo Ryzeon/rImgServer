101bc185de6c0e3ed63c884609c618672ae6ac8b
Ryzeon <alex.ramon2004@gmail.com>
2021-05-31 15:41:12 -0500

    Updated?? src/main/java/me/ryzeon/img/rest/rIndexController.java

---
+++ src/main/java/me/ryzeon/img/rest/rIndexController.java
@@ -0,0 +1,10 @@
+package me.ryzeon.img.rest;/**
+ * Created by Ryzeon
+ * Project: rImgServer
+ * Date: 31/05/2021 @ 15:41
+ * Twitter: @Ryzeon_ 😎
+ * Github:  github.ryzeon.me
+ */
+    
+public class rIndexController {
+}
