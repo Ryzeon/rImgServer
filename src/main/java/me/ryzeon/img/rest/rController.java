ad654c1a264dbee95d556c5c3f8aa53b1836ce71
Ryzeon <alex.ramon2004@gmail.com>
2021-05-31 13:15:28 -0500

    Updated  src/main/java/me/ryzeon/img/rest/rController.java

--- src/main/java/me/ryzeon/img/rest/rController.java
+++ src/main/java/me/ryzeon/img/rest/rController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Github:  github.ryzeon.me
  */
 
-@RestController
+@Controller
 public class rController {
 
     @GetMapping(value = "/")
