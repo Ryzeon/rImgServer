4dbabffb6df840e071433687215b5076d22a25a8
Ryzeon <alex.ramon2004@gmail.com>
2021-05-31 12:49:25 -0500

    Updated?? src/main/java/me/ryzeon/img/rest/

---
+++ src/main/java/me/ryzeon/img/rest/RestController.java
@@ -0,0 +1,10 @@
+package me.ryzeon.img.rest;/**
+ * Created by Ryzeon
+ * Project: rImgServer
+ * Date: 31/05/2021 @ 12:49
+ * Twitter: @Ryzeon_ 😎
+ * Github:  github.ryzeon.me
+ */
+    
+public class RestController {
+}
