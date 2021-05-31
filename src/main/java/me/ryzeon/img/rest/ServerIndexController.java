10cae840c485cc7490cf904bf03d856b1d8c8821
Ryzeon <alex.ramon2004@gmail.com>
2021-05-31 12:57:11 -0500

    UpdatedR  src/main/java/me/ryzeon/img/rest/rController.java -> src/main/java/me/ryzeon/img/rest/ServerIndexController.java

--- src/main/java/me/ryzeon/img/rest/ServerIndexController.java
+++ src/main/java/me/ryzeon/img/rest/ServerIndexController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 
 @Controller
-public class rController {
+public class ServerIndexController {
 
     @GetMapping("/")
     public String index() {
