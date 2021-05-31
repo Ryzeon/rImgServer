dabfe23c8d4d0009e1c00a9746fbbdf77f79d254
Ryzeon <alex.ramon2004@gmail.com>
2021-05-31 13:26:58 -0500

    UpdatedA src/main/java/me/ryzeon/img/web/WebAppConfig.java

---
+++ src/main/java/me/ryzeon/img/web/WebAppConfig.java
@@ -0,0 +1,12 @@
+package me.ryzeon.img.web;
+
+/**
+ * Created by Ryzeon
+ * Project: rImgServer
+ * Date: 31/05/2021 @ 13:26
+ * Twitter: @Ryzeon_ 😎
+ * Github:  github.ryzeon.me
+ */
+
+public class WebAppConfig {
+}
