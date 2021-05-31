1c6a06cf1b01234a7e1f9d8739900027e7da0230
Ryzeon <alex.ramon2004@gmail.com>
2021-05-31 14:56:23 -0500

    UpdatedA src/main/java/me/ryzeon/img/rest/rPageErrorController.java

---
+++ src/main/java/me/ryzeon/img/rest/rPageErrorController.java
@@ -0,0 +1,12 @@
+package me.ryzeon.img.rest;
+
+/**
+ * Created by Ryzeon
+ * Project: rImgServer
+ * Date: 31/05/2021 @ 14:56
+ * Twitter: @Ryzeon_ 😎
+ * Github:  github.ryzeon.me
+ */
+
+public class rPageErrorController {
+}
