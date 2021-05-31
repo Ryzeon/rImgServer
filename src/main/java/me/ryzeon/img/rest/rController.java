689583ece43cf16c3e82dbd72cf22042a8535a5a
Ryzeon <alex.ramon2004@gmail.com>
2021-05-31 17:46:35 -0500

    Updated  .gitignore   pom.xml   src/main/java/me/ryzeon/img/Lang.java D  src/main/java/me/ryzeon/img/LoggableDispatcherServlet.java   src/main/java/me/ryzeon/img/rest/rController.java   src/main/resources/application.properties

--- src/main/java/me/ryzeon/img/rest/rController.java
+++ src/main/java/me/ryzeon/img/rest/rController.java
@@ -1,10 +1,21 @@
 package me.ryzeon.img.rest;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import me.ryzeon.img.Lang;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Random;
 
 
 /**
@@ -15,13 +26,51 @@ import javax.servlet.http.HttpServletRequest;
  * Github:  github.ryzeon.me
  */
 
-@RestController
+@Controller
 public class rController {
 
-    @PostMapping("/upload")
-    public String upload(HttpServletRequest request) {
-        System.out.println("REQUEST :V");
-        System.out.println(request);
-        return "NOSE K PONER CA PE XD";
+    private final Random random = new Random();
+
+    private int getRandomId() {
+        return random.nextInt(848484);
+    }
+
+    public File crateImgFile(String fileName) {
+        File file = new File("img/" + fileName);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return file;
+    }
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public ResponseEntity<?> upload(HttpServletRequest request) {
+        String key = request.getHeader("key");
+        if (!key.equals(Lang.API_KEY.getValue())) {
+            return new ResponseEntity<>("Invalid Key", HttpStatus.BAD_REQUEST);
+        }
+        String filename = "";
+        try {
+            MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+            Iterator<String> it = multipartRequest.getFileNames();
+            MultipartFile multipart = multipartRequest.getFile(it.next());
+            String fileName = request.getHeader("name").replace(".png", "");
+            fileName = fileName + getRandomId() + ".png";
+
+            byte[] bytes = multipart.getBytes();
+            BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(crateImgFile(fileName)));
+            stream.write(bytes);
+            stream.close();
+            JSONObject
+            return new ResponseEntity<>(filename, HttpStatus.OK);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Error to upload file", HttpStatus.BAD_REQUEST);
+        }
     }
 }
