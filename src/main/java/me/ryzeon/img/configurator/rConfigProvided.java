c9b8ece82145c6ae3abdab1bbb0e4792717ef88b
Ryzeon <alex.ramon2004@gmail.com>
2021-05-31 21:26:27 -0500

    Updated  src/main/java/me/ryzeon/img/configurator/rConfigProvided.java

--- src/main/java/me/ryzeon/img/configurator/rConfigProvided.java
+++ src/main/java/me/ryzeon/img/configurator/rConfigProvided.java
@@ -4,7 +4,6 @@ import me.ryzeon.img.image.ImageHelper;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by Ryzeon
