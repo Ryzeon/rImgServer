f63abe81a740a78f0ab744192d68912447e8f2ac
Ryzeon <alex.ramon2004@gmail.com>
2021-05-31 21:13:41 -0500

    Updated  src/main/java/me/ryzeon/img/rest/rIndexController.java

--- src/main/java/me/ryzeon/img/rest/rIndexController.java
+++ src/main/java/me/ryzeon/img/rest/rIndexController.java
@@ -1,7 +1,6 @@
 package me.ryzeon.img.rest;
 
 import me.ryzeon.img.Lang;
-import me.ryzeon.img.rImgServer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
