608f3429b264a63d50bb2bac5ecbc275c3ccfebf
Ryzeon <alex.ramon2004@gmail.com>
2021-05-31 19:00:43 -0500

    Updated  src/main/java/me/ryzeon/img/image/ImageHelper.java

--- src/main/java/me/ryzeon/img/image/ImageHelper.java
+++ src/main/java/me/ryzeon/img/image/ImageHelper.java
@@ -22,7 +22,7 @@ public class ImageHelper {
     private static String path() {
         Path path = Paths.get(".");
         Path abosoultePath = path.toAbsolutePath();
-        
+        return abosoultePath  + ""
     }
 
     public static int getRandomId() {
